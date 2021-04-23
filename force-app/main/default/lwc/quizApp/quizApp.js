672f55328cdd55845faeeb656eeb144da1090f43
Maxim Sh <maxshev2000@gmail.com>
2021-04-23 07:55:00 +0300

    quiz

--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -1,3 +1,40 @@
 import { LightningElement } from 'lwc';
 
-export default class QuizApp extends LightningElement {}
+export default class QuizApp extends LightningElement {
+    myQuestions = [
+        {
+            id: "Question1",
+            question: "Which of the following is not a template loop",
+            answers: {
+                a:"for:each",
+                b:"iterator",
+                c:"map loop"
+            },
+            correctAnswer:"c"
+        },
+        {
+            id:"Question2",
+            question:"Which of the file is invald in LWC component folder?",
+            answers:{
+                a:".svg",
+                b:".apex",
+                c:".js"
+            },
+            correctAnswer:"b"
+        },
+        {
+            id:"Question3",
+            question:"Which one of the following is not a directive?",
+            answers:{
+                a:"for:each",
+                b:"if:true",
+                c:"@track"
+            },
+            correctAnswer:"c"
+        }
+    ]
+
+    changeHandler (event){
+        
+    }
+}
